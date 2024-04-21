43291fa7cf95917387dc5c976ff2d22af6183327
Rasmi Jati <jatirasmi65@gmail.com>
2024-04-21 08:05:41 +0545

    create endpoints using extension class and method

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,76 +1,8 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-const string GetGameEndpointName = "GetGame";
 
-List<GameDto> games = [
-    new (
-        1,
-        "Street Fighter II",
-         "Fighting",
-         19.9M,
-         new DateOnly(1992,7,15)
-         ),
-    new (
-               2,
-        "Final Fantansy XIV",
-         "RolePlaying",
-         59.99M,
-         new DateOnly(1990,5,20)
-         )
-];
-
-
-// Defining endpoints
-
-//GET /games
-app.MapGet("games",()=> games);
-
-//GET /games/id
-
-app.MapGet("games/{id}",(int id)=>{
-     GameDto? game = games.Find(game=> game.Id == id);
-     return game is null? Results.NotFound() : Results.Ok(game);
-     }).WithName("GetGameEndpointName");
-
-// POST /games
-app.MapPost("games",(CreateGameDto newGame)=>{
-   GameDto game= new GameDto(
-    games.Count +1,
-    newGame.Name,
-    newGame.Genre,
-    newGame.Price,
-    newGame.ReleaseDate
-   );
-   games.Add(game);
-   return Results.CreatedAtRoute("GetGameEndpointName",new { id = game.Id , game});
-   });
-
-// PUT /games/id
-app.MapPut("games/{id}",(int id , UpdateGameDto updatedGame)=>{
-
-    var index = games.FindIndex(game=> game.Id == id);
-
-    if (index == -1){
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDto(
-      id,
-      updatedGame.Name,
-      updatedGame.Genre,
-      updatedGame.Price,
-      updatedGame.ReleaseDate
-    );   
-    return Results.NoContent();
-
-});
-
-// DELETE /games/id
-app.MapDelete("games/{id}", (int id) =>{
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4f6d9741a92f2552cdc17773d7c4623a09cfe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95ad370dd68d5f860ec013eae72c486cf61a9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
