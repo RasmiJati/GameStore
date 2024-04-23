43a1c0eaa741dfa180e793d149644337ec75a80a
Rasmi Jati <jatirasmi65@gmail.com>
2024-04-23 11:12:50 +0545

    create database with migration

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,7 +4,7 @@ using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 var conString = builder.Configuration.GetConnectionString("GameStore");
-// var conString = "Data Source = GameStore.db";  --> without using appsettings.jsom
+
 builder.Services.AddSqlite<GameStoreContext>(conString);
 
 var app = builder.Build();
